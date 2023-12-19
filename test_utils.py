8b732f4263e79dd89c20faa53b4d71ce3c211285
Ben Johnson <johnsonb@ucar.edu>
2023-12-19 15:18:38 -0700

    Update test_utils with notes about unittest

--- test_utils.py
+++ test_utils.py
@@ -2,7 +2,7 @@
 from __future__ import print_function
 
 import unittest
-from utils import database
+from utils import cron, database
 
 class TestCron(unittest.TestCase):
     pass
@@ -12,5 +12,28 @@ class TestDatabase(unittest.TestCase):
     def test_create(self):
         database.create()
 
+    
+
 if __name__ == '__main__':
     unittest.main()
+
+#setUp()
+    
+#tearDown()
+    
+#setUpClass()
+    
+#tearDownClass()
+
+# assertEqual(a, b)
+# assertNotEqual(a, b)
+# assertTrue(x)
+# assertFalse(x)
+# assertIs(a, b)
+# assertIsNot(a, b)
+# assertIsNone(x)
+# assertIsNotNone(x)
+# assertIn(a, b)
+# assertNotIn(a, b)
+# assertIsInstance(a, b)
+# assertNotIsInstance(a, b)
