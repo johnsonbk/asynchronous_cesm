7083d2ff63b7ad1c41f6f1ab53721fb494b2130f
Ben Johnson <johnsonb@ucar.edu>
2024-04-24 14:01:44 -0600

    Add comments to write_time_to_file.py

--- write_time_to_file.py
+++ write_time_to_file.py
@@ -4,6 +4,11 @@ from __future__ import print_function
 import datetime
 import sys
 
+# This script is called by test_cron. This script writes the current time to 
+# a new line in a text file whenever it is called. test_cron calls the script
+# once per minute and if the number of lines written to the file matches the
+# test duration.
+
 current_time = datetime.datetime.now()
 
 output_filename = sys.argv[1]
